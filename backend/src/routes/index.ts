e420ea29229296d3f6ff5176d39dae91ab515588
Lucas Davi <lucasdavisan@gmail.com>
2020-04-30 12:16:43 -0300

    Configuring Prettier tool.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello Gostack!' }));
+routes.get('/', (request, response) =>
+	response.json({ message: 'Hello Gostack!' }),
+);
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,5 +6,5 @@ const app = express();
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started on http://localhost:3333');
+	console.log('Server started on http://localhost:3333');
 });
