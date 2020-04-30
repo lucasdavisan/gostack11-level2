ae4885052bd90b415b3d5650ec8778a1befd36be
Lucas Davi <lucasdavisan@gmail.com>
2020-04-30 11:38:09 -0300

    Configuring ESLint.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Gostack!' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello Gostack!' }));
 
 app.listen(3333, () => {
   console.log('Server started on http://localhost:3333');
