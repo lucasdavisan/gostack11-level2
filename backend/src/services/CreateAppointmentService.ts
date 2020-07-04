e6c7c9e4103f06a65c0843980ed58f0389c4ebd1
Lucas Davi <lucasdavisan@gmail.com>
2020-07-04 12:27:40 -0300

    Repository of TypeORM.

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -1,13 +1,8 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-
-/**
- * [x] Receiving information.
- * [x] Handling errors / exceptions.
- * [x] Repository access
- */
 
 interface RequestDTO {
 	provider: string;
@@ -15,16 +10,12 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
-	private appointmentsRepository: AppointmentsRepository;
+	public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+		const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-	constructor(appointmentsRepository: AppointmentsRepository) {
-		this.appointmentsRepository = appointmentsRepository;
-	}
-
-	public execute({ date, provider }: RequestDTO): Appointment {
 		const appointmentDate = startOfHour(date);
 
-		const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+		const findAppointmentInSameDate = await appointmentsRepository.findByDate(
 			appointmentDate,
 		);
 
@@ -32,10 +23,12 @@ class CreateAppointmentService {
 			throw Error('This appointment is already booked');
 		}
 
-		const appointment = this.appointmentsRepository.create({
+		const appointment = appointmentsRepository.create({
 			provider,
 			date: appointmentDate,
 		});
+
+		await appointmentsRepository.save(appointment);
 
 		return appointment;
 	}
