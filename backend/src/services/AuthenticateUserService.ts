58c04b4e2360cf2b5cc424ee0e619e30c4236f32
Lucas Davi <lucasdavisan@gmail.com>
2020-07-05 20:26:04 -0300

    Generating JWT token.

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
 	user: User;
+	token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,15 @@ class AuthenticateUserService {
 			throw new Error('Incorrect email/password combination.');
 		}
 
-		return { user };
+		const token = sign({}, '70472cd15147eeb1a510b0f1af122f98', {
+			subject: user.id,
+			expiresIn: '1d',
+		});
+
+		return {
+			user,
+			token,
+		};
 	}
 }
 
