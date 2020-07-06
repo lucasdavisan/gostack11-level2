24500887ed2465ac36493f0148c0585cf61b5827
Lucas Davi <lucasdavisan@gmail.com>
2020-07-06 17:57:57 -0300

    Authenticated routes.

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -5,13 +5,19 @@ import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const appointmentsRouter = Router();
 
 /** Route: should always be concerned with receiving the requisiton,
  *  call another file and return a response. Otherwise we must be
  * 	abstract in a Service. */
 
+appointmentsRouter.use(ensureAuthenticated);
+
 appointmentsRouter.get('/', async (request, response) => {
+	console.log(request.user);
+
 	const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 	const appointments = await appointmentsRepository.find();
 
